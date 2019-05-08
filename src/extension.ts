1d9651393a9f236af1c3e04eb52188ee010d25eb
Joey <513358386@qq.com>
2019-05-08 10:49:42 +0800

    Delete print

--- src/extension.ts
+++ src/extension.ts
@@ -19,7 +19,115 @@ const insertText = (val: string) => {
   editor.edit((editBuilder) => {
     editBuilder.replace(range, val);
   });
+};
+
+const getFilePathSuffix = (editor: any) => {
+  const fullPath = editor.document.fileName;
+  const fileName = fullPath.replace(/^.*[\\\/]/, '');
+  const [_, suffix] = fileName.split('.');
+  return suffix;
+};
+
+const getAllLogStatements = (fileSuffix: string, document: any, documentText: any) => {
+  let logStatements = [];
+
+  let logRegex: any;
+  switch (fileSuffix) {
+    case 'js':
+    case 'jsx':
+    case 'ts':
+      logRegex = /console.(log)\((.*)\);?/g;
+      break;
+    case 'py': // python
+    case 'rb': // ruby
+      logRegex = /print\((.*)\);?/g;
+      break;
+    case 'java': // java
+      logRegex = /System.out.print\((.*)\);?/g;
+      break;
+    default:
+      logRegex = /console.(log)\((.*)\);?/g;
+      break;
+  }
+
+  let match;
+  while (match = logRegex.exec(documentText)) {
+    let matchRange =
+      new vscode.Range(
+        document.positionAt(match.index),
+        document.positionAt(match.index + match[0].length)
+      );
+
+    let emptyLineRange =
+      new vscode.Range(
+        document.positionAt(match.index + match[0].length),
+        document.positionAt(match.index + match[0].length + 1)
+      );
+
+    if (!matchRange.isEmpty)
+      logStatements.push(matchRange);
+    logStatements.push(emptyLineRange);
+  }
+
+  return logStatements;
 }
+
+
+const logExist = (editor: any, fileSuffix: string, document: any, documentText: any) => {
+  let logStatements = [];
+
+  let logRegex: any;
+  switch (fileSuffix) {
+    case 'js':
+    case 'jsx':
+    case 'ts':
+      logRegex = /console.(log)\((.*)\);?/g;
+      break;
+    case 'py': // python
+    case 'rb': // ruby
+      logRegex = /print\((.*)\);?/g;
+      break;
+    case 'java': // java
+      logRegex = /System.out.print\((.*)\);?/g;
+      break;
+    default:
+      logRegex = /console.(log)\((.*)\);?/g;
+      break;
+  }
+
+  let selections = [];
+
+  let match;
+  while (match = logRegex.exec(documentText)) {
+    let matchRange =
+      new vscode.Range(
+        document.positionAt(match.index),
+        document.positionAt(match.index + match[0].length)
+      );
+
+    const selection = new vscode.Selection(
+      document.positionAt(match.index), document.positionAt(match.index + match[0].length),
+    )
+
+    selections.push(selection);
+
+    if (!matchRange.isEmpty)
+      logStatements.push(matchRange);
+  }
+
+  editor.selections = selections;
+  // console.log('editor.selections: ', editor.selections);
+
+  return selections.length > 0;
+}
+
+// const deleteFoundLogStatements = (editor: any, workspaceEdit: any, docUri: any, logs: any) => {
+//   vscode.workspace.applyEdit(workspaceEdit).then(() => {
+//     logs.length > 1
+//       ? vscode.window.showInformationMessage(`${logs.length} console.logs deleted`)
+//       : vscode.window.showInformationMessage(`${logs.length} console.log deleted`);
+//   });
+// }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -39,9 +147,7 @@ export function activate(context: vscode.ExtensionContext) {
     const text = editor.document.getText(selection);
 
     // Get file type
-    const fullPath = editor.document.fileName;
-    const fileName = fullPath.replace(/^.*[\\\/]/, '');
-    const [_, suffix] = fileName.split('.');
+    const suffix = getFilePathSuffix(editor);
     if (suffix) {
       let logToInsert = '';
       switch (suffix) {
@@ -72,7 +178,25 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
+  const deletePrintStatement = vscode.commands.registerCommand('extension.deletePrintStatement', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) { return; }
+
+    const document = editor.document;
+    const documentText = editor.document.getText();
+    const suffix = getFilePathSuffix(editor);
+    let workspaceEdit = new vscode.WorkspaceEdit();
+
+    if (logExist(editor, suffix, document, documentText)) {
+      vscode.commands.executeCommand('editor.action.deleteLines')
+        .then(() => {
+          console.log('DELETEED!')
+        });
+    }
+  });
+
   context.subscriptions.push(insertPrintStatement);
+  context.subscriptions.push(deletePrintStatement);
 }
 
 // this method is called when your extension is deactivated
